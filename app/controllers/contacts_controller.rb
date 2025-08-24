8b815c2238d3e651bc0740754757285a51fe898d
Sergio Oseko <osk.sergio@gmail.com>
2025-08-24 06:45:44 -0300

    feat: POST com associações belongs_to

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -46,6 +46,6 @@ class ContactsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def contact_params
-      params.expect(contact: [ :name, :email, :birthdate ])
+      params.expect(contact: [ :name, :email, :birthdate, :kind_id ])
     end
 end
